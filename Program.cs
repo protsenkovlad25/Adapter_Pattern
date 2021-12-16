0b2b5ee8259c156be37ec5b1da138a1b5cd1d5d3
Vlad <vladislav.procenko.2002@mail.ru>
2021-12-16 09:29:41 +0700

    Changes

--- Program.cs
+++ Program.cs
@@ -11,16 +11,16 @@ namespace BookShop_Adapter
         {
             Catalog catalog = new Catalog();
 
-            CsvReading csv = new CsvReading("Files/CSV.csv");
+            CsvReading csv = new CsvReading(@"C:\Users\79825\source\repos\BookShop_Adapter\Files\CSV.csv");
             catalog.BookRead = csv;
             catalog.ReadingBooks();
 
-            JsonReading json = new JsonReading("Files/JSON.json");
+            JsonReading json = new JsonReading(@"C:\Users\79825\source\repos\BookShop_Adapter\Files\JSON.json");
             JsonToCsvAdapter jsonAdapter = new JsonToCsvAdapter(json);
             catalog.BookRead = jsonAdapter;
             catalog.ReadingBooks();
 
-            XmlReading xml = new XmlReading("Files/XML.xml");
+            XmlReading xml = new XmlReading(@"C:\Users\79825\source\repos\BookShop_Adapter\Files\XML.xml");
             XmlToCsvAdapter xmlAdapter = new XmlToCsvAdapter(xml);
             catalog.BookRead = xmlAdapter;
             catalog.ReadingBooks();
